0cb254fa8a8f0e04932b0004dd7be29b4b99357a
unknown <AKazakov@ton-note.(none)>
2012-06-26 01:03:57 +0400

    maven project structure

---
+++ src/main/java/org/gt/42goals/App.java
@@ -0,0 +1,13 @@
+package org.gt.42goals;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
